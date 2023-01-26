d21bd23411975d02ac8549612f4a190ee2ba53a0
angelique-df <faye.angelique.d@gmail.com>
2023-01-26 21:12:39 +0000

    Update Image.java

--- src/Image.java
+++ src/Image.java
@@ -1,4 +1,29 @@
 import java.util.Arrays;
 
 public class Image {
+    public static void main(String[] args) {
+        int[][] image = creerImage();
+    }
+    public static int[][] creerImage() {
+        System.out.println("Entrez la largeur de l'image");
+        int largeur = Terminal.lireInt();
+        System.out.println("Entrez la hauteur de l'image");
+        int hauteur = Terminal.lireInt();
+        int[][] image = new int[hauteur][largeur];
+        for (int i = 0; i < image.length; i++) {
+            for (int j = 0; j < image[0].length; j++) {
+                System.out.println("Entrez le niveau de gris de l'image");
+                int nvGris = -1;
+                try {
+                    nvGris = Terminal.lireInt();
+                } catch (TerminalException te) {
+                    System.out.println("Erreur : le niveau de gris doit être une valeur numérique;");
+                }
+                if (nvGris < 0 || nvGris > 255)
+                    throw new NiveauGrisException();
+                image[i][j] = nvGris;
+            }
+        }
+        return image;
+    }
 }
